abdf817dac52e5facd4798b2b7396aa7d3fffb87
Yagdra <43428247+Yagdra-nsk@users.noreply.github.com>
2020-09-14 02:35:54 +0700

    Remove macros SUCCESS

--- Lab1/main.c
+++ Lab1/main.c
@@ -1,30 +1,20 @@
 #include <stdio.h>
 #include <pthread.h>
 
-#define SUCCESS 0
-
 void *thread(void *param) {
     for (int i = 0; i < 10; ++i) {
         printf("Thread %d\n", i);
     }
-    pthread_exit((void *) SUCCESS);
+    pthread_exit((void *)0);
 }
 
 int main() {
-
     pthread_t id;
-
-    int status = pthread_create(&id, NULL, thread, NULL);
-
-
-    if (status != SUCCESS) {
-        pthread_exit((void *) status);
+    if (pthread_create(&id, NULL, thread, NULL)) {
+        return 1;
     }
-
     for (int i = 0; i < 10; ++i) {
         printf("Main Thread %d\n", i);
     }
-
-
-    pthread_exit((void *) SUCCESS);;
+    pthread_exit((void *)0);;
 }
