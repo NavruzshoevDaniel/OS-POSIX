7ed66462641228ede95155e744058e252c3baa60
Yagdra-nsk <Akela.Klimov@yandex.ru>
2020-09-17 10:53:54 +0700

    Add lab7 "Calculate PI"

---
+++ Lab7/cmake-build-debug/CMakeFiles/Lab7.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lab7.dir/main.c.o"
+  "Lab7"
+  "Lab7.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Lab7.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Lab7/main.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+#define ITERATIONS_NUM 20000
+int THREADS_NUM = 0;
+
+struct Param{
+    int offset;
+    double partial_sum;
+};
+
+void* execute_thread(void* args){
+    int i;
+    double pi = 0.0;
+    struct Param* param = (struct Param*)args;
+
+    for(i = param->offset; i < ITERATIONS_NUM; i += THREADS_NUM) {
+        pi += 1.0/(i*4.0 + 1.0);
+        pi -= 1.0/(i*4.0 + 3.0);
+    }
+
+    printf("thread-%d's partial_sum = %f\n", param->offset, pi);
+    ((struct Param*)args)->partial_sum = pi;
+
+    return args;
+}
+
+int main(int argc, char* argv[]){
+    if(argc != 2){
+        printf("Bad number of arguments!\n");
+        return 1;
+    }
+
+    THREADS_NUM = atoi(argv[1]);
+    pthread_t threads[THREADS_NUM];
+    int stat;
+    int i;
+
+    struct Param* params;
+    params = (struct Param*)malloc(sizeof(struct Param)*THREADS_NUM);
+
+    for(i = 0; i < THREADS_NUM; i++){
+        params[i].offset = i;
+        stat = pthread_create(&threads[i], NULL, execute_thread, &(params[i]));
+        if(stat != 0){
+            printf("Error while creating thread-%d\n", i);
+        }
+    }
+
+    double result = 0.0;
+    for(i = 0; i < THREADS_NUM; i++){
+        stat = pthread_join(threads[i], NULL);
+        if(stat != 0){
+            printf("Error while joining thread-%d\n", i);
+        }
+        result += (params[i]).partial_sum;
+    }
+
+    result = result*4.0;
+    printf("pi done = %.15g \n", result);
+
+    return 0;
+}
