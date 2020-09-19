bc5d96263083a1de9cb8e0ec461fddb46796057b
Yagdra-nsk <Akela.Klimov@yandex.ru>
2020-09-19 21:19:46 +0700

    Modify lab4 with alarm

--- Lab4/main.c
+++ Lab4/main.c
@@ -1,46 +1,53 @@
 #include <stdio.h>
 #include <unistd.h>
-#include <time.h>
+#include <signal.h>
 #include <pthread.h>
 
+int flag_to_finish = 0;
 void* func();
+void sigAlarm(int sig);
 
 int main(){
     pthread_t  pthread;
-    int i;
     void* res;
     char *str[] = {"Operating ", "System ", "Lab ", "4 ", "Test\n"};
-    if(pthread_create(&pthread, NULL, func, str) != 0){
+    if(pthread_create(&pthread, NULL, func, str)){
+        printf("ERROR, cannot create thread!\n");
         return 1;
     }
-    sleep(2);
+    signal(SIGALRM, sigAlarm);
+    alarm(2);
+
     pthread_cancel(pthread);
     if(pthread_join(pthread, &res)){
         printf("ERROR, cannot join thread!\n");
+        return 1;
     }
-
     if(res == PTHREAD_CANCELED){
         printf("Thread canceled\n");
     }
-
     return 0;
+}
+
+void sigAlarm(int sig){
+    if(sig == SIGALRM){
+        flag_to_finish = 1;
+    }
 }
 
 void* func(void* ptr){
     char** str;
     int i;
     str = (char **)ptr;
-    time_t curr;
-    curr = time(NULL);
-    while(1){
-        pthread_testcancel();//Cancelling point
-        if(curr < time(NULL)){
-            //Print one string every second
-            curr = time(NULL);
-            for(i = 0; i < 5; i++){
-                printf("%s", str[i]);
-            }
+    //Not allow this fucking printf() to cancel thread...
+    pthread_setcancelstate(PTHREAD_CANCEL_DISABLE, NULL);
+    while(flag_to_finish == 0){
+        for(i = 0; i < 5; i++){
+            printf("%s", str[i]);
         }
     }
+    pthread_setcancelstate(PTHREAD_CANCEL_ENABLE, NULL);
+    pthread_testcancel();
+    pthread_exit((void*)0);
 }
 
