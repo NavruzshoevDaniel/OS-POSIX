d6961306a855bcaf8b9940dd349c702eea37efbe
NavruzshoevDaniel <den.navruzshoev@gmail.com>
2020-10-28 00:18:42 +0700

    not ready

--- Lab17/cmake-build-debug/CMakeFiles/Lab17.dir/DependInfo.cmake
+++ Lab17/cmake-build-debug/CMakeFiles/Lab17.dir/DependInfo.cmake
@@ -4,9 +4,9 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_C
-  "C:/Users/Public/Documents/Clion_Progects/OS-POSIX/Lab7/main.c" "C:/Users/Public/Documents/Clion_Progects/OS-POSIX/Lab7/cmake-build-debug/CMakeFiles/Lab7.dir/main.c.obj"
+  "/Users/daniel/JetBrains/CLionProjects/OS-POSIX/Lab17/main.c" "/Users/daniel/JetBrains/CLionProjects/OS-POSIX/Lab17/cmake-build-debug/CMakeFiles/Lab17.dir/main.c.o"
   )
-set(CMAKE_C_COMPILER_ID "GNU")
+set(CMAKE_C_COMPILER_ID "AppleClang")
 
 # The include file search paths:
 set(CMAKE_C_TARGET_INCLUDE_PATH
---
+++ Lab17/cmake-build-debug/CMakeFiles/Lab17.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lab17.dir/main.c.o"
+  "Lab17"
+  "Lab17.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Lab17.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
